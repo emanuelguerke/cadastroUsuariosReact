5c6d7c6dc1c909100a30179ab0244b66796c5e93
emanuelguerke <emanuelguerke@users.noreply.github.com>
2019-06-06 17:35:34 -0300

    Update Logo.jsx

--- cadastroUsuarios/frontend/src/components/template/Logo.jsx
+++ cadastroUsuarios/frontend/src/components/template/Logo.jsx
@@ -1,6 +1,6 @@
 import './Logo.css'
 import React from 'react'
-import logo from '../../imgs/logo.png'
+import logo from '../../imgs/logo2.png'
 
 export default props =>
     <aside className="logo">
