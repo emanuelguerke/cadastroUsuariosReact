fa83c338ff0e8fa438aeeb753f90a0b6bfb7dc38
emanuelguerke <emanuelguerke@users.noreply.github.com>
2019-06-14 02:23:53 -0300

    Create Home.jsx

---
+++ cadastroUsuarios/frontend/src/components/home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon='home' tittle='Início' subtit="Cadastro de usuários" >
+        <div className="display-4">Bem Vindo</div>
+        <hr />
+        <p className="mb-0">Sistema de cadastro de Usuarios em React</p>
+    </Main>
